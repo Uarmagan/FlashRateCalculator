4349a55985b2dafa9c46043e370459d043daa193
Ugur Armagan <ugurarmagan93@gmail.com>
2019-02-10 20:56:28 -0600

    switched getElementbyId with querySelector and used class instead of ID

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", () => {
-  const pickupField = document.getElementById('pickup');
-  const dropoffField = document.getElementById('dropoff');
+
+  const pickupField = document.querySelector('.pickup');
+  const dropoffField = document.querySelector('.dropoff');
 
   const autocompletePickup = autocompleteForm(pickupField);
 
@@ -10,7 +11,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   autocompletePickup.addListener('place_changed', () => {
     const pickupInfo = autocompletePickup.getPlace();
-    console.log(pickupInfo)
+    const dropoffInfo = autocompleteDropoff.getPlace();
+    console.log(pickupInfo);
+    console.log(dropoffInfo);
   })
 });
 
