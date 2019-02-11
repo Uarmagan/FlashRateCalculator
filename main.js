08ac715a282d58a8a09fae934e95adea5d2478e1
Ugur Armagan <ugurarmagan93@gmail.com>
2019-02-10 19:05:47 -0600

    made autocomplete form method

--- main.js
+++ main.js
@@ -1,31 +1,25 @@
 'use strict';
 
 document.addEventListener("DOMContentLoaded", () => {
-  require('dotenv').config();
-  const pickupAutocompleteForm = document.getElementById('pickup');
-  const dropoffAutocompleteForm = document.getElementById('dropoff');
-  const API_KEY = process.env.GO_FLASH_API; 
+  const pickupField = document.getElementById('pickup');
+  const dropoffField = document.getElementById('dropoff');
 
-  // const script = document.createElement('script'); 
-  // script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&libraries=places`;
-  // document.body.append(script);
-  console.log(`api key is ${API_KEY}`)
+  const autocompletePickup = autocompleteForm(pickupField);
 
-  const autocompletePickup = new google.maps.places.Autocomplete((pickupAutocompleteForm), {
-      latLngBounds:{north: 41, south: 52, west: 87, east: 23} ,
-      strictBounds:true,
-      componentRestrictions: {country: 'us'}
-    });
+  const autocompleteDropoff = autocompleteForm(dropoffField);
 
-    const autocompleteDropoff = new google.maps.places.Autocomplete((dropoffAutocompleteForm), {
-      latLngBounds:{north: 41, south: 52, west: 87, east: 23},
-      strictBounds:true,
-      componentRestrictions: {country: 'us'}
-    });
-
-    autocompletePickup.addListener('place_changed', () => {
-      const pickupInfo = autocompletePickup.getPlace();
-      console.log(pickupInfo)
-    })
+  autocompletePickup.addListener('place_changed', () => {
+    const pickupInfo = autocompletePickup.getPlace();
+    console.log(pickupInfo)
+  })
 });
+
+const autocompleteForm  = (formField) => {
+  const selection = new google.maps.places.Autocomplete((formField), {
+    latLngBounds:{north: 41, south: 52, west: 87, east: 23} ,
+    strictBounds:true,
+    componentRestrictions: {country: 'us'}
+  });
+  return selection;
+}
 
