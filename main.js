4698840c56055e68d0675ee9a0cb6fc5234ce92f
Ugur Armagan <ugurarmagan93@gmail.com>
2019-02-10 21:19:19 -0600

    added event listener for click on calc button

--- main.js
+++ main.js
@@ -4,25 +4,33 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const pickupField = document.querySelector('.pickup');
   const dropoffField = document.querySelector('.dropoff');
+  const calcButton = document.querySelector('.calculate');
 
   const autocompletePickup = autocompleteForm(pickupField);
 
   const autocompleteDropoff = autocompleteForm(dropoffField);
 
-  autocompletePickup.addListener('place_changed', () => {
+  calcButton.addEventListener('click', (event) => {
+    event.preventDefault();
     const pickupInfo = autocompletePickup.getPlace();
     const dropoffInfo = autocompleteDropoff.getPlace();
     console.log(pickupInfo);
     console.log(dropoffInfo);
-  })
+  });
 });
 
-const autocompleteForm  = (formField) => {
+const autocompleteForm = (formField) => {
   const selection = new google.maps.places.Autocomplete((formField), {
-    latLngBounds:{north: 41, south: 52, west: 87, east: 23} ,
-    strictBounds:true,
-    componentRestrictions: {country: 'us'}
+    latLngBounds: {
+      north: 41,
+      south: 52,
+      west: 87,
+      east: 23
+    },
+    strictBounds: true,
+    componentRestrictions: {
+      country: 'us'
+    }
   });
   return selection;
 }
-
