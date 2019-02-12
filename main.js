12db5aa1bd142b7a00388b9827dfed409925f492
Ugur Armagan <ugurarmagan93@gmail.com>
2019-02-11 20:46:25 -0600

    distance matrix added to display the distance between the two points

--- main.js
+++ main.js
@@ -6,18 +6,39 @@ document.addEventListener("DOMContentLoaded", () => {
   const dropoffField = document.querySelector('.dropoff');
   const calcButton = document.querySelector('.calculate');
 
-  const autocompletePickup = autocompleteForm(pickupField);
+  let pickupInfo;
+  let dropoffInfo;
 
+  const autocompletePickup = autocompleteForm(pickupField);
   const autocompleteDropoff = autocompleteForm(dropoffField);
+  var service = new google.maps.DistanceMatrixService();
 
   calcButton.addEventListener('click', (event) => {
     event.preventDefault();
-    const pickupInfo = autocompletePickup.getPlace();
-    const dropoffInfo = autocompleteDropoff.getPlace();
+    pickupInfo = autocompletePickup.getPlace();
+    dropoffInfo = autocompleteDropoff.getPlace();
     console.log(pickupInfo);
     console.log(dropoffInfo);
-  });
+
+    var origin1 = pickupInfo.formatted_address;
+    var destinationA = dropoffInfo.formatted_address;
+
+    const distanceService = new google.maps.DistanceMatrixService();
+    service.getDistanceMatrix(
+      {
+        origins: [origin1],
+        destinations: [destinationA],
+        travelMode: 'DRIVING',
+        unitSystem: google.maps.UnitSystem.IMPERIAL
+      }, callback);
+    });
 });
+
+function callback(response, status) {
+  let test = response.rows[0].elements[0].distance.text
+  let element = document.querySelector('.distance');
+  element.textContent += test
+}
 
 const autocompleteForm = (formField) => {
   const selection = new google.maps.places.Autocomplete((formField), {
