2895801d43ad767aa894046e6a54aa58f16bd0ca
Ugur Armagan <ugurarmagan93@gmail.com>
2019-02-17 17:00:01 -0600

    quick cleanup

--- main.js
+++ main.js
@@ -10,10 +10,6 @@ const pickupField = document.querySelector('.pickup');
 const dropoffField = document.querySelector('.dropoff');
 const calcButton = document.querySelector('.calculate');
 const garage = "5320 W Lawrence Ave #211, Chicago, IL 60630, USA";
-var distances = {}
-var pickupInfo;
-var dropoffInfo;
-
 
 document.addEventListener("DOMContentLoaded", () => {
   
@@ -24,31 +20,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
   calcButton.addEventListener('click', async (event) => {
     event.preventDefault();
-    pickupInfo =  autocompletePickup.getPlace();
-    dropoffInfo =  autocompleteDropoff.getPlace();
-    console.log(pickupInfo);
-    console.log(dropoffInfo);
+    const pickupInfo =  await autocompletePickup.getPlace();
+    const dropoffInfo =  await autocompleteDropoff.getPlace();
 
-    const distanceMatrix = new DistanceMatrixService(pickupInfo.formatted_address, dropoffInfo.formatted_address, garage)
+    const distanceMatrix = new DistanceMatrixService(pickupInfo.formatted_address, dropoffInfo.formatted_address, garage);
+
     const pickup =  pickupInfo.formatted_address
     const dropoff = dropoffInfo.formatted_address
-    console.log(pickup)
-    console.log(dropoff)
-
 
     const gtp = await distanceMatrix.getDistance(garage, pickup);
     const ptd = await distanceMatrix.getDistance(pickup, dropoff);
     const dtg = await distanceMatrix.getDistance(dropoff, garage);
 
-    await console.log(gtp)
-    await console.log(ptd)
-    // distances = {
-    //   garageToPickup: await distanceMatrix.getDistance(garage, pickup),
-    //   pickupToDropoff: await distanceMatrix.getDistance(pickup, dropoff),
-    //   dropoffToGarage: await distanceMatrix.getDistance(dropoff, garage)
-    // }
-  
-    console.log(distances)
+    console.log(`Garage to Pickup is ${gtp}`);
+    console.log(`Pickup to Geparture is ${ptd}`)
+    console.log(`Departure to Garage is ${dtg}`)
   });
 });
 
--- services/distanceMatrix.js
+++ services/distanceMatrix.js
@@ -1,6 +1,6 @@
 "use strict";
 export default class DistanceMatrixService {
-    constructor(pickup, dropoff, garage){
+    constructor(pickup, dropoff, garage) {
         this.pickup = pickup;
         this.dropoff = dropoff;
         this.garage = garage;
@@ -9,22 +9,17 @@ export default class DistanceMatrixService {
 
     getDistance(arg1, arg2) {
         return new Promise((resolve, reject) => {
-            try{
+            try {
                 this.distance.getDistanceMatrix({
-                origins: [arg1],
-                destinations: [arg2],
-                travelMode: 'DRIVING',
-                unitSystem: google.maps.UnitSystem.IMPERIAL
-            }, response => {
-                console.log(response)
-                resolve(response.rows[0].elements[0].distance.text)
-            }
-                
-                );
-            } catch(e){
-                console.error(`error: ${e.message}`)
+                    origins: [arg1],
+                    destinations: [arg2],
+                    travelMode: 'DRIVING',
+                    unitSystem: google.maps.UnitSystem.IMPERIAL
+                }, response => resolve(response.rows[0].elements[0].distance.text));
+            } catch (e) {
+                reject(console.error(`error: ${e.message}`));
             }
         });
     }
-    
+
 }
